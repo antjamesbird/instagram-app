02676278c3965870f549e57e27be05b4efb0105e
Anthony Bird <anthony@responsive.co.za>
2019-05-26 18:09:51 +0200

    final app

--- client/store.js
+++ client/store.js
@@ -1,4 +1,4 @@
-import { createStore, compse } from 'redux';
+import { createStore, compose } from 'redux';
 import { syncHistoryWithStore} from 'react-router-redux';
 import { browserHistory } from 'react-router';
 
@@ -14,8 +14,17 @@ const defaultState = {
   comments
 };
 
-const store = createStore(rootReducer, defaultState);
+const enhancers = compose(window.devToolsExtension ? window.devToolsExtension() : (f) => f)
+// const enhancers = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
+const store = createStore(rootReducer, defaultState, enhancers);
 
 export const history = syncHistoryWithStore(browserHistory, store);
+
+if(module.hot) {
+    module.hot.accept('./reducers/',() => {
+      const nextRootReducer = require('./reducers/index').default;
+      store.replaceReducer(nextRootReducer);
+    });
+  }
 
 export default store;
