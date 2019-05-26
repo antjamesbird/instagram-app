ec1319e6fb923e0b415854ccf76208334547a40d
Anthony Bird <anthony@responsive.co.za>
2019-05-26 15:15:47 +0200

    add comment component

--- client/components/Single.js
+++ client/components/Single.js
@@ -1,18 +1,23 @@
 import React, { Component } from 'react';
+import Photo from './Photo';
+import Comments from './Comments';
+class Single  extends Component {
+    render() {
+        const { postId } = this.props.params;
 
-class Single extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {  }
-    }
-    render() { 
+        const i = this.props.posts.findIndex((post) => post.code === postId);
+        const post = this.props.posts[i];
+
+        const postComments = this.props.comments[postId] || [];
+
         return (
-            <div className="single-photo">
-                I'm the single
-            </div>
-        );
+        <div className="single-photo">
+            <Photo i={i} post={post} {...this.props} />
+            <Comments postComments={postComments} {...this.props} />
+        </div>
+        )
     }
 }
- 
-export default Single;
 
+
+export default Single;
